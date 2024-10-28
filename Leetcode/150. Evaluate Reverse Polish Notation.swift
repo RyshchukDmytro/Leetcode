4aac11a5e3db621ec2f71378f48de89afe5a4756
Dmytro <Dmytro.Ryshchuk@icloud.com>
2024-10-27 19:43:36 -0500

    Add   Code  Testcase  Test Result Test Result 150. Evaluate Reverse Polish Notation

---
+++ Leetcode/150. Evaluate Reverse Polish Notation.swift
@@ -0,0 +1,34 @@
+//
+//  150. Evaluate Reverse Polish Notation.swift
+//  Leetcode
+//
+//  Created by Dmytro Ryshchuk on 10/27/24.
+//
+
+import Foundation
+
+class Solution {
+    func evalRPN(_ tokens: [String]) -> Int {
+        var arr = [Int]()
+
+        for elem in tokens {
+            if let num = Int(elem) {
+                arr.append(num)
+            } else {
+                let second = arr.removeLast()
+                let first = arr.removeLast()
+                if elem == "+" {
+                    arr.append(first + second)
+                } else if elem == "-" {
+                    arr.append(first - second)
+                } else if elem == "/" {
+                    arr.append(first / second)
+                } else if elem == "*" {
+                    arr.append(first * second)
+                }
+            }
+        }
+
+        return arr[0]
+    }
+}
