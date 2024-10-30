bbc0a53d787cbfd17898a245cbecf4cc93a81017
Dmytro <Dmytro.Ryshchuk@icloud.com>
2024-10-29 23:10:11 -0500

    1

---
+++ Leetcode/1. Two Sum.swift
@@ -0,0 +1,23 @@
+//
+//  1. Two Sum.swift
+//  Leetcode
+//
+//  Created by Dmytro Ryshchuk on 10/29/24.
+//
+
+import Foundation
+
+class TwoSum {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int: Int]()
+
+        for (i, val) in nums.enumerated() {
+            if let j = dict[target - nums[i]] {
+                return [j, i]
+            }
+            dict[val] = i
+        }
+
+        return []
+    }
+}
