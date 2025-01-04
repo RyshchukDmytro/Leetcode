bb7dd6f84260235b428d72dfd81920d4abba660d
Dmytro <Dmytro.Ryshchuk@icloud.com>
2025-01-04 10:01:17 -0600

    1929

---
+++ Leetcode/1929. Concatenation of Array.swift
@@ -0,0 +1,14 @@
+//
+//  1929. Concatenation of Array.swift
+//  Leetcode
+//
+//  Created by Dmytro Ryshchuk on 1/4/25.
+//
+
+import Foundation
+
+class ConcatenationOfArray {
+    func getConcatenation(_ nums: [Int]) -> [Int] {
+        nums + nums
+    }
+}
